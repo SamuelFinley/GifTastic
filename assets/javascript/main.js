d292955e6c5e56082a1d2441ff0cb7f5f82555ab
Edward Finley <biged621@gmail.com>
2018-03-24 06:56:40 -0400

    got stuff done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,56 +2,39 @@ $(document).ready(() => {
 
 
   let topics = {};
-  let config = {
+  const config = {
     apiKey: '&api_key=7IvkmXh1DwPHidKwAZmY6eRK8eSWkYMk&limit=',
     URL: 'https://api.giphy.com/v1/gifs/search?q=',
     limit: 10,
   }
-
-  $('#submit').click(() => {
-    $('#mainContent > h1').css('display', 'none')
-    $('#mainContent').css('display', 'block')
-    let value = $('#topicInput').prop('value');
-    $('.gif').remove();
-    let queryURL = config.URL + value + config.apiKey + config.limit;
-    topics[value] = {
-      name: value,
-      url: queryURL,
-    }
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      let gifs = response.data;
-      gifs.forEach(gif => {
-        create ('gif', true, {srcURL: gif.images.fixed_height_small_still.url, rating: gif.rating, srcAlt: gif.images.original.url});
-      });
-    });
-    search(value);
-    //window.localStorage.setItem('topics', JSON.stringify(topics));
-    //JSON.parse(localStorage.getItem('user'));
-  });
-
-  function create (clss, bool, obj) {
+  
+  function create (tag1, tag2, clss, bool, obj) {
+    let element = document.createElement(tag1);
+    let child = document.createElement(tag2);
     if (bool) {
-      let child = document.createElement('IMG');
-      let element = document.createElement('BUTTON');
       let close = document.createElement('BUTTON');
       element.setAttribute('class', clss);
-      child.setAttribute("src", obj.srcURL);
-      child.setAttribute("alt", "oh no!");
+      child.setAttribute("src", obj.images.fixed_height_small_still.url);
+      element.setAttribute("value", obj.images.fixed_height_small_still.url);
+      child.setAttribute("alt", obj.images.fixed_height_small.url);
       element.innerHTML = 'rating: ' + obj.rating + '<br><br>';
       document.getElementsByClassName('gifArea')[0].appendChild(element);
       element.appendChild(child);
       element.onclick = function () {
-        console.log(obj.srcAlt)
-        $('#alt').css('display', 'flex')
-        $('#pic1').attr("src", obj.srcAlt)
-        console.log(document.getElementById('pic1'))
+        $('#alt').css('display', 'flex');
+        $('#pic1').attr("src", obj.images.original.url);
       }
+        $(".gif").hover(
+            function() {
+              $(this.childNodes[3]).attr("src", this.childNodes[3].alt);
+            },
+            function() {
+              //console.log(this.value);
+              $(this.childNodes[3]).attr("src", this.value);
+            }                         
+        );                  
+
     } else {
-      let element = document.createElement('DIV');
-      let child = document.createElement('BUTTON');
       let child2 = document.createElement('BUTTON');
       element.setAttribute('class', clss);
       element.setAttribute('id', obj.value);
@@ -64,37 +47,57 @@ $(document).ready(() => {
       child.onclick = function () {
         this.parentNode.remove();
       }
-      console.log(obj.value)
       return {element: element, child: child, child2: child2, id: obj.value};
     }
   }
 
-  $('#close').click(function () {
-    $('#alt').css('display', 'none')
-  });
-  function search (value) {
-    let obj = create ('topic', false, {value: value});
-    obj.child2.onclick = function () {
-      let name = $(this).attr('value')
-      if (document.getElementsByClassName('gif')[0]) {
-        $('.gif').remove();
-      }
-      console.log(name)
+
+  function api (url) {
     $.ajax({
-      url: topics[name].url,
+      url: url,
       method: "GET"
     }).then(function(response) {
       let gifs = response.data;
       gifs.forEach(gif => {
-        console.log(gif.rating)
-        create ('gif', true, {srcURL: gif.images.fixed_height_small_still.url, rating: gif.rating, srcAlt: gif.images.original.url});
+        create ('BUTTON', 'IMG', 'gif', true, gif);
       });
     });
+  };
+
+  function search (value) {
+    let obj = create ('DIV', 'BUTTON', 'topic', false, {value: value});
+    obj.child2.onclick = function () {
+      let name = $(this).attr('value')
+      if (document.getElementsByClassName('gif')[0]) {
+        $('.gif').remove();
+      };
+      api(topics[name].url);
     };
     document.getElementsByClassName('topicArea')[0].appendChild(obj.element);
-    console.log(document.getElementById(obj.id))
     document.getElementById(obj.id).appendChild(obj.child2);
     document.getElementById(obj.id).appendChild(obj.child);
   };
+
+  $('#submit').click(() => {
+    let value = $('#topicInput').prop('value');
+    let queryURL = config.URL + value + config.apiKey + config.limit;
+    if (!topics[value]) {
+      $('.gif').remove();
+      $('#mainContent > h1').css('display', 'none');
+      $('#mainContent').css('display', 'block');
+      topics[value] = {
+        name: value,
+        url: queryURL,
+      };
+      api(queryURL);
+      search(value);
+    };
+    //window.localStorage.setItem('topics', JSON.stringify(topics));
+    //JSON.parse(localStorage.getItem('user'));
+  });
+
+  $('#close').click(function () {
+    $('#alt').css('display', 'none');
+  });
 })
     
